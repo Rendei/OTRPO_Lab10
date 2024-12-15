e2ffbd455a9560bd6462030238cc0452ac9514d0
Арсений Ш <cenz1217@gmail.com>
2024-12-15 21:08:10 +0500

    added new metrics

--- exporter.py
+++ exporter.py
@@ -8,25 +8,36 @@ EXPORTER_HOST = os.getenv('EXPORTER_HOST', '0.0.0.0')
 EXPORTER_PORT = int(os.getenv('EXPORTER_PORT', 8080))
 
 # Создаём метрики
-cpu_usage = Gauge('cpu_usage_percent', 'CPU Usage in Percent')
-memory_total = Gauge('memory_total', 'Total system memory in bytes')
+cpu_usage_per_core = Gauge('cpu_usage_percent_per_core', 'CPU Usage in Percent per Core', ['core'])
+cpu_usage_total = Gauge('cpu_usage_percent', 'CPU Usage in Percent')
+
 memory_used = Gauge('memory_used', 'Used system memory in bytes')
-disk_total = Gauge('disk_total', 'Total disk space in bytes')
-disk_used = Gauge('disk_used', 'Used disk space in bytes')
+
+disk_total = Gauge('disk_total', 'Total disk space in bytes', ['disk'])
+disk_used = Gauge('disk_used', 'Used disk space in bytes', ['disk'])
 
 def collect_metrics():
     # Получаем информацию о процессорах
-    cpu_usage.set(psutil.cpu_percent())
+    for i, percent in enumerate(psutil.cpu_percent(percpu=True)):
+        cpu_usage_per_core.labels(core=f'core_{i}').set(percent)
+
+    cpu_usage_total.set(psutil.cpu_percent())
 
     # Получаем информацию о памяти
-    memory = psutil.virtual_memory()
-    memory_total.set(memory.total)
+    memory = psutil.virtual_memory()    
     memory_used.set(memory.used)
 
     # Получаем информацию о дисках
-    disk = psutil.disk_usage('/')
-    disk_total.set(disk.total)
-    disk_used.set(disk.used)
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            print(partition.device)
+            disk = psutil.disk_usage(partition.mountpoint)
+            disk_total.labels(disk=partition.device).set(disk.total)
+            disk_used.labels(disk=partition.device).set(disk.used)
+        except PermissionError:
+            # Игнорируем разделы, к которым нет доступа
+            continue
 
 if __name__ == '__main__':
     start_http_server(EXPORTER_PORT, addr=EXPORTER_HOST)
